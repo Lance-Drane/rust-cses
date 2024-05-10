125120ee14426acfaf34df8aea50413d8296d21c
Lance-Drane <ldraneutk@gmail.com>
2024-05-09 21:41:09 -0400

    counting numbers 2 solution
    Signed-off-by: Lance-Drane <ldraneutk@gmail.com>

--- src/bin/search_collecting_numbers_2.rs
+++ src/bin/search_collecting_numbers_2.rs
@@ -42,11 +42,15 @@ impl UnsafeScanner<'_> {
 ///
 /// On each round, you go through the array from left to right and collect as many numbers as possible. What will be the total number of rounds?
 ///
+/// Given m operations that swap two numbers in the array, your task is to report the number of rounds after each operation.
+///
 /// <b>Input</b>
 ///
-/// The first input line has an integer n: the array size.
+/// The first line has two integers n and m: the array size and the number of operations.
 ///
 /// The next line contains n integers x<sub>1</sub>,x<sub>2</sub>,...,x<sub>n</sub>: the numbers in the array.
+///
+/// Finally, there are m lines that describe the operations. Each line has two integers a and b: the numbers at positions a and b are swapped.
 ///
 /// <b>Output</b>
 ///
@@ -55,25 +59,75 @@ impl UnsafeScanner<'_> {
 /// <b>Constraints</b>
 ///
 /// <ul>
-/// <li>1 ≤ n ≤ 2 * 10<sup>5</sup></li>
+/// <li>1 ≤ n,m ≤ 2 * 10<sup>5</sup></li>
+/// <li>1 ≤ a,b ≤ n</li>
 /// </ul>
 fn solve<W: std::io::Write>(mut scan: UnsafeScanner, out: &mut W) {
-    let n: u32 = scan.token();
-    let mut arr = vec![0; (n + 1) as usize];
+    let n: usize = scan.token();
+    let m: u32 = scan.token();
+    let mut position_arr = vec![0; n + 1];
+    let mut num_arr = vec![0; n + 1];
 
-    for (index, number) in (0..n).map(|idx| (idx, scan.token::<usize>())) {
+    for (index, number) in (1..=n).map(|idx| (idx, scan.token::<usize>())) {
         unsafe {
-            *arr.get_unchecked_mut(number) = index;
+            *position_arr.get_unchecked_mut(number) = index;
+            *num_arr.get_unchecked_mut(index) = number;
         }
     }
 
-    writeln!(
-        out,
-        "{}",
-        arr.windows(2).filter(|w| w[1] < w[0]).count() + 1
-    )
-    .unwrap();
+    let mut rounds = position_arr.windows(2).filter(|w| w[1] < w[0]).count() + 1;
+    let mut pairs: Vec<(usize, usize)> = Vec::with_capacity(4);
+
+    for _ in 0..m {
+        let l_idx: usize = scan.token();
+        let r_idx: usize = scan.token();
+
+        if l_idx == r_idx {
+            writeln!(out, "{rounds}").unwrap();
+            continue;
+        }
+
+        let l_num = unsafe { *num_arr.get_unchecked(l_idx) };
+        let r_num = unsafe { *num_arr.get_unchecked(r_idx) };
+        num_arr.swap(l_idx, r_idx);
+
+        let (min, max) = if l_num < r_num {
+            (l_num, r_num)
+        } else {
+            (r_num, l_num)
+        };
+        if min != 1 {
+            pairs.push((min - 1, min));
+        }
+        match max - min {
+            1 => pairs.push((min, max)),
+            _ => pairs.extend([(min, min + 1), (max - 1, max)]),
+        }
+        if max != n {
+            pairs.push((max, max + 1));
+        }
+
+        rounds -= pairs
+            .iter()
+            .filter(|(l, r)| position_arr[*l] > position_arr[*r])
+            .count();
+        position_arr.swap(l_num, r_num);
+        rounds += pairs
+            .iter()
+            .filter(|(l, r)| position_arr[*l] > position_arr[*r])
+            .count();
+
+        pairs.clear();
+        writeln!(out, "{rounds}").unwrap();
+    }
 }
+
+// 4 2 1 5 3 = 3 2 5 1 4
+// 4 1 2 5 3 = 2 3 5 1 4
+// 3 1 2 5 4 = 3 5 4 1 2
+// 3 2 1 5 4 = 3 5 4 2 1
+
+// 3 1 5 2 4
 
 // entrypoints //
 
@@ -98,37 +152,16 @@ mod test {
     #[test]
     fn test_example() {
         let input = b"\
-5
+5 3
 4 2 1 5 3
+2 3
+1 5
+2 3
 ";
         let target = b"\
+2
 3
-";
-
-        test(input, target);
-    }
-
-    #[test]
-    fn test_asc() {
-        let input = b"\
-10
-1 2 3 4 5 6 7 8 9 10
-";
-        let target = b"\
-1
-";
-
-        test(input, target);
-    }
-
-    #[test]
-    fn test_desc() {
-        let input = b"\
-10
-10 9 8 7 6 5 4 3 2 1
-";
-        let target = b"\
-10
+4
 ";
 
         test(input, target);
@@ -137,8 +170,9 @@ mod test {
     #[test]
     fn test_one() {
         let input = b"\
+1 1
 1
-1
+1 1
 ";
         let target = b"\
 1
