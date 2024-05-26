9492fad4430d047199af4b5d0d6662c3b3e63438
Lance-Drane <ldraneutk@gmail.com>
2024-05-25 21:18:55 -0400

    book shop solution
    Signed-off-by: Lance-Drane <ldraneutk@gmail.com>

--- src/bin/dynamic_book_shop.rs
+++ src/bin/dynamic_book_shop.rs
@@ -24,7 +24,7 @@ impl UnsafeScanner<'_> {
     /// Use "turbofish" syntax `token::<T>()` to select data type of next token.
     ///
     /// # Panics
-    /// Panics if there's no more tokens or if the token cannot be parsed as T.
+    /// Panics if there are no more tokens or if the token cannot be parsed as T.
     pub fn token<T: std::str::FromStr>(&mut self) -> T {
         unsafe {
             self.buf_iter
@@ -38,47 +38,45 @@ impl UnsafeScanner<'_> {
 
 // problem //
 
-/// There is a line that goes through the points p<sub>1</sub>=(x<sub>1</sub>,y<sub>1</sub>) and p<sub>2</sub>=(x<sub>2</sub>,y<sub>2</sub>). There is also a point p<sub>3</sub>=(x<sub>3</sub>,y<sub>3</sub>).
+/// You are in a book shop which sells n different books. You know the price and number of pages of each book.
 ///
-/// Your task is to determine whether p<sub>3</sub> is located on the left or right side of the line or if it touches the line when we are looking from p<sub>1</sub> to p<sub>2</sub>.
+/// You have decided that the total price of your purchases will be at most x. What is the maximum number of pages you can buy? You can buy each book at most once.
 ///
 /// <b>Input</b>
 ///
-/// The first input line has an integer t: the number of tests.
+/// The first input line contains two integers n and x: the number of books and the maximum total price.
 ///
-/// After this, there are t lines that describe the tests. Each line has six integers: x<sub>1</sub>, y<sub>1</sub>, x<sub>2</sub>, y<sub>2</sub>, x<sub>3</sub> and y<sub>3</sub>.
+/// The next line contains n integers h<sub>1</sub>,h<sub>2</sub>,...,h<sub>n</sub>: the price of each book.
+///
+/// The next line contains n integers s<sub>1</sub>,s<sub>2</sub>,...,s<sub>n</sub>: the number of pages of each book.
 ///
 /// <b>Output</b>
 ///
-/// For each test, print "LEFT", "RIGHT" or "TOUCH".
+/// Print one integer: the maximum number of pages.
 ///
 /// <b>Constraints</b>
 ///
 /// <ul>
-/// <li>1 ≤ t ≤ 10<sup>5</sup></li>
-/// <li>-10<sup>9</sup> ≤ x<sub>1</sub>, y<sub>1</sub>, x<sub>2</sub>, y<sub>2</sub>, x<sub>3</sub>, y<sub>3</sub> ≤ 10<sup>9</sup></li>
-/// <li>x<sub>1</sub> != x<sub>2</sub>, y<sub>1</sub> != y<sub>2</sub> </li>
+/// <li>1 ≤ n ≤ 1000</li>
+/// <li>1 ≤ x ≤ 10<sup>5</sup></li>
+/// <li>1 ≤ h<sub>i</sub>,s<sub>i</sub> ≤ 10<sup>6</sup></li>
 /// </ul>
 fn solve<W: std::io::Write>(mut scan: UnsafeScanner, out: &mut W) {
-    let t: u32 = scan.token();
+    let n: u16 = scan.token();
+    let max_price: usize = scan.token();
 
-    for _ in 0..t {
-        let x1: i64 = scan.token();
-        let y1: i64 = scan.token();
-        let x2: i64 = scan.token();
-        let y2: i64 = scan.token();
-        let x3: i64 = scan.token();
-        let y3: i64 = scan.token();
+    let prices: Vec<usize> = (0..n).map(|_| scan.token()).collect();
+    let pages: Vec<u32> = (0..n).map(|_| scan.token()).collect();
 
-        out.write_all(
-            match ((y3 - y1) * (x2 - x1) - (x3 - x1) * (y2 - y1)).cmp(&0) {
-                std::cmp::Ordering::Less => b"RIGHT\n",
-                std::cmp::Ordering::Equal => b"TOUCH\n",
-                std::cmp::Ordering::Greater => b"LEFT\n",
-            },
-        )
-        .unwrap();
+    let mut cache = vec![0; max_price + 1];
+
+    for (price, page) in prices.into_iter().zip(pages.into_iter()) {
+        for total in (price..=max_price).rev() {
+            cache[total] = std::cmp::max(cache[total], cache[total - price] + page);
+        }
     }
+
+    writeln!(out, "{}", cache[max_price]).unwrap();
 }
 
 // entrypoints //
@@ -104,15 +102,12 @@ mod test {
     #[test]
     fn test_example() {
         let input = b"\
-3
-1 1 5 3 2 3
-1 1 5 3 4 1
-1 1 5 3 3 2
+4 10
+4 8 5 3
+5 12 8 1
 ";
         let target = b"\
-LEFT
-RIGHT
-TOUCH
+13
 ";
 
         test(input, target);
